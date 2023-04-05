dd8312f6d1e86b72346b4e795505f564ebc13339
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-04-05 20:17:56 +0530

    .

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-
 mongoose
   .connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DATABASE has been connected");
-  });  // catch part is handeled by unhandeled promise rejection
+  }); // catch part is handeled by unhandeled promise rejection
