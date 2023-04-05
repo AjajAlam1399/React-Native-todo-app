1e3091161a24bf3d196a3177e5fa45528bb930d2
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-04-05 20:24:44 +0530

    .

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // console.log(typeof(process.env.MONGODB_URI))
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(`${process.env.MONGODB_URI}`, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
