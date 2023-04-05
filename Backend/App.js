83ba3da8e159c8d19f73b369493dad7046de8251
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-04-05 16:14:25 +0530

    .

--- Backend/App.js
+++ Backend/App.js
@@ -19,9 +19,9 @@ app.use("/api/v1", User);
 
 app.use(ErrorMiddleware);
 
-app.get('/',(reqs,resp)=>{
-resp.send("server is working fine");
-})
+// app.get('/',(reqs,resp)=>{
+// resp.send("server is working fine");
+// })
 
 module.exports = app;
 
